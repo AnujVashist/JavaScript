2cf93ea7d74dca66896a18aa14168d7d8020e341
Anuj Vashist <126752118+AnujVashist@users.noreply.github.com>
2024-07-16 13:49:26 +0000

    let, const, var - variables in js

--- basics_js/test.js
+++ basics_js/test.js
@@ -1,1 +1,17 @@
 console.group("Hello World");
+
+const accountId = 225349
+let accountEmail = 'Demo@gamil.com'
+var accountPassward = "123456"
+accountCity = 'jaipur'
+// accountId = 395289
+
+accountEmail = 'Do@gamil.com'
+accountPassward = "654321"
+accountCity = 'pune'
+let accountState ; //no value in this variable 
+
+
+console.table(
+    [accountId,accountEmail,accountPassward, accountCity, accountState]
+)

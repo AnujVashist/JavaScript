1236dfd197bca51649b71687ef479217e817bccd
Anuj Vashist <126752118+AnujVashist@users.noreply.github.com>
2024-07-20 19:23:29 +0000

    scopes  and controlflow in javascript

---
+++ basics_js/controlFlow.js
@@ -0,0 +1,173 @@
+const temperature = 40;
+
+// if (temperature <= 40) {
+//   console.log("less than 50 degree");
+// } else {
+//   console.log("temperature is greater than 50");
+// }
+
+// <, >, <=, >=, ==, ===, !=
+
+const score = 200;
+if (score > 100) {
+  const power = "fly";
+  //console.log(`user power is : ${power}`);
+}
+//show the error in case of conmst or let because both are used in the block scope but var is used and no error show because var is globle scope .
+//console.log(`user power is : ${power}`);
+
+// implicity scope or maanliziye yhe scope hai only for one line code excution only
+//const balance = 1000;
+// if (balance > 500) console.log("implicity scope test");
+// nesting conditions
+const balance = 899;
+
+// if (balance < 500) {
+//   console.log("less then 500");
+// } else if (balance < 750) {
+//   console.log("less then 750");
+// } else if (balance < 900) {
+//   console.log("less then 900");
+// } else {
+//   console.log("less then 1200");
+// }
+
+// ************************************
+
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromgoogle = false;
+const loggedInFromgEmail = true;
+
+// if (userLoggedIn && debitCard && 2 ===2) {
+//   console.log("Allow to buy course");
+// }
+
+// if(loggedInFromgoogle || loggedInFromgEmail){
+//     console.log('Log In Successfully')
+// }
+
+// switch statement
+// syntax of switch statement
+// switch (key) {
+//   case value:
+//     break;
+
+//   default:
+//     break;
+// }
+
+const month = 3;
+
+//(break) example : jese ki case 3 match ho gya or break nhi hai to case3 ki value ya console ki value k sath baki bchi hui values bi dedeta hai sath m default case ki bi .
+
+// switch (month) {
+//   case 1:
+//     console.log("Jan");
+//     break;
+//   case 2:
+//     console.log("feb");
+//     break;
+//   case 3:
+//     console.log("mar");
+//     break;
+//   case 4:
+//     console.log("apr");
+//     break;
+//   case 5:
+//     console.log("may");
+//     break;
+//   case 6:
+//     console.log("june");
+//     break;
+//   case 7:
+//     console.log("july");
+//     break;
+//   case 8:
+//     console.log("aug");
+//     break;
+//   case 9:
+//     console.log("sep");
+//     break;
+//   case 10:
+//     console.log("oct");
+//     break;
+//   case 11:
+//     console.log("nov");
+//     break;
+//   case 12:
+//     console.log("dec");
+//     break;
+//   default:
+//     console.log("default case");
+//     break;
+// }
+
+const months = "apr";
+
+//(break) example : jese ki case 3 match ho gya or break nhi hai to case3 ki value ya console ki value k sath baki bchi hui values bi dedeta hai sath m default case ki bi .
+
+// switch (months) {
+//   case "Jan":
+//     console.log("Jan");
+//     break;
+//   case "feb":
+//     console.log("feb");
+//     break;
+//   case "mar":
+//     console.log("mar");
+//     break;
+//   case "apr":
+//     console.log("apr");
+//     break;
+//   default:
+//     console.log("default case");
+//     break;
+// }
+
+// truthy values
+// Manle na ki value shi hai or galat hai us ko hi truthy and falsy values bolte hai
+
+const Email = "h@gmail.com";
+
+// if (Email) {
+//   console.log("got user email");
+// } else {
+//   console.log(`don't have user email`);
+// }
+
+// falsy values : false, 0, -0, null, undefined, bigint (0n) is falsy value, "" (empty),  NaN.
+// Truthy values : "0", 'false', " " space is a value in the  string, [] Array empty, {} Object empty, function(){}
+
+const Array1 = [];
+// if (Array1.length === 0) {
+//   console.log("Array is empty");
+// }
+
+const emptyObject = {};
+// if (Object.keys(emptyObject).length === 0) {
+//   console.log("Object is empty");
+// }
+
+// false == 0
+// false == ''
+// 0 == ''
+
+// Nullish coalescing Operator (??)  : null , undefined
+
+let val1;
+// val1 = 5 ?? 10; // 5
+//val1 = null ?? 10; // 10
+//val1 = undefined ?? 10; //
+
+val1 = null ?? 15 ?? 20; // 15 only center value show
+
+//console.log(val1);
+
+// ternary operator
+
+// condition ? true :  false
+
+const iceTeaPrice = 100;
+
+iceTeaPrice <= 80 ? console.log(`less than 80`) : console.log(`more than 80`);
--- basics_js/scopes.js
+++ basics_js/scopes.js
@@ -7,10 +7,140 @@ let b = 200;
 if (true) {
   var a = 10;
   let b = 20;
-  console.log("Inner value ", b);
+  //console.log("Inner value ", b);
   const c = 30;
 }
 
-console.log(b);
+//console.log(b);
 // console.log(b);
 // console.log(c);
+
+// paraent function
+function one() {
+  const UserName = "macBook";
+
+  // child function
+  function Two() {
+    const website = "youtub.com";
+
+    //console.log(UserName);
+  }
+
+  //console.log(website);
+
+  Two();
+}
+
+one();
+
+//  child function access the parent function variables   . note;
+
+if (true) {
+  const Username = "MicroSoft";
+
+  if (Username === "MicroSoft") {
+    const website = " youtube";
+    // console.log(Username + website);
+  }
+  //console.log(website);
+}
+//console.log(Username);
+
+// ++++++++++++++++++++ interesting ++++++++++
+
+function addOne(num) {
+  return num + 1;
+}
+addOne(5);
+// variables hold all value in the js . like function ,json value etc varibales are very powerfull in the js .
+
+//this type of code is exprestions.
+
+//Cannot access 'addTwo' before initialization
+//because not direct use the function. hold value in the variable that is not access the addTwo() function value and this is same as arrow function .
+//addTwo(5);
+
+const addTwo = function (num) {
+  return num + 2;
+};
+
+// arrow function es6 and this keyword.
+//current context of this user is {} inside the {} curry brases
+const user = {
+  name: "Ram",
+  email: "emial@gmail.com",
+  welcomeMessages: function () {
+    console.log(`${this.name} , welcome to website`);
+    console.log(this); // this keyword output is {name: 'sam',email: 'emial@gmail.com',welcomeMessages: [Function: welcomeMessages]}
+  },
+};
+
+//===== this keyword used in the  object not in the function.
+
+// user.welcomeMessages();
+// user.name = "sam";
+// user.welcomeMessages();
+
+//console.log(this); // output of this keyword is empty object {}. current context oe value of this console is empty . no globle scope value in this code .
+
+// arrow function
+
+function one() {
+  let Username = "samy";
+  //console.log(this.Username);
+}
+//one(); // undefined . why Undefined out .solution in line no . 78 .
+
+const Two = function two() {
+  let Username = "samy";
+  console.log(this.Username);
+};
+
+//arrow function
+
+// const chai = () => {
+//   let username = "sam";
+//   console.log(this);
+// };
+
+// chai();
+
+//basic arrow function
+
+// const addNum = (num1, num2) => {
+//   return num1 + num2;
+// }; // explicitly return return keyword lgana hai
+
+//const addNum = (num1, num2) => num1 + num2; // implicit return nhi lgana hai
+//const addNum = (num1, num2) => (num1 + num2); // both are same
+// object pass in the arrow function
+const addNum = (num1, num2) => ({
+  name: "sam",
+});
+//console.log(addNum(3, 4));
+
+// Immediately Invoked Function Expressions(IIFE)
+
+function chai() {
+  console.log("DB CONNECTED");
+}
+//chai();
+
+// globle scopes value poluted / polution
+
+// what is IIFE ? Jo function immedatly excute ho ja ye . globle scope k polution se problem hoti hai aur delared value se to iife ka use krte hai .
+// use polution ko htane k liye iife ka use hota hai sath m immidetly use or excute k liye bi use hota hai.
+// syntax  () ()
+(function chaiANDcode() {
+  // named iife
+  console.log("DB CONNECTED Two");
+})();
+// using arrow function and last m semi-column lgana must hai iife k case m .
+(() => {
+  console.log("DB CONNECTED three");
+})();
+
+// parameter pass
+((name) => {
+  console.log(`DB CONNECTED three,  ${name}`);
+})("macbook");

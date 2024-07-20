5ec86ad7ffc2b73fff4d557e78486e2fec6cfd20
Anuj Vashist <126752118+AnujVashist@users.noreply.github.com>
2024-07-20 08:01:17 +0000

    scopes in javascript

---
+++ basics_js/scopes.js
@@ -0,0 +1,16 @@
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+let b = 200;
+
+if (true) {
+  var a = 10;
+  let b = 20;
+  console.log("Inner value ", b);
+  const c = 30;
+}
+
+console.log(b);
+// console.log(b);
+// console.log(c);
